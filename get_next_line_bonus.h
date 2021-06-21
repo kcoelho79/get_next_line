e7d4b3d8529b17ef5834c2c582ffa847707d8e67
Your Name <keber.amg79@gmail.com>
2021-06-21 16:12:01 -0300

    get_next_line #SP42 - Bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kde-oliv <kde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/10 10:35:13 by kde-oliv          #+#    #+#             */
+/*   Updated: 2021/06/18 16:33:45 by kde-oliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define OPEN_MAX 1024
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+char	*ft_cp_blockstr(char *save, size_t end);
+
+#endif
